47f1ce4da05034403c8a3c8ebb9202b24e9e8a48
akira-yp <yog.puripuri@gmail.com>
2022-09-19 16:57:17 +0900

    環境変数をtfvarsに移行

--- variables.tf
+++ variables.tf
@@ -1,12 +1,10 @@
 variable "prefix" {
   description = "Projcet name given as a prefix"
   type        = string
-  default     = "tf-test"
 }
 variable "vpc_cidr" {
   description = "The CIDR block of VPC"
   type        = string
-  default     = "172.26.0.0/16"
 }
 variable "rds_password" {
   type = string
